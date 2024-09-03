73b8e406865e2f4b498ed5e0b7409198aa23a14c
Hilmi AM <imiia75775@proton.me>
2024-09-03 13:41:18 +0700

    path

---
+++ path.mjs
@@ -0,0 +1,9 @@
+import path from "path";
+
+const file = "/home/noir/contoh.txt";
+
+console.info(path.dirname(file));
+console.info(path.basename(file));
+console.info(path.extname(file));
+console.info(path.delimiter)
+console.info(path.sep)
